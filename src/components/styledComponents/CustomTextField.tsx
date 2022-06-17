ea8be65e1350660d883d70d794276f2fec90f9f9
boening <Marcel.Boening@adesso.de>
2022-06-17 10:06:44 +0200

    changed styling for text fields - unsafe

--- src/components/styledComponents/CustomTextField.tsx
+++ src/components/styledComponents/CustomTextField.tsx
@@ -4,17 +4,11 @@ import { styled } from "@mui/material/styles";
 import * as React from "react";
 
 const MyTextField = styled(TextField)({
-  "& input:valid + fieldset": {
-    borderColor: "green",
-    borderWidth: 2,
-  },
-  "& input:invalid + fieldset": {
-    borderColor: "red",
-    borderWidth: 2,
-  },
-  "& input:valid:focus + fieldset": {
-    borderLeftWidth: 6,
-    padding: "4px !important", // override inline-style
+  "& fieldset": {
+    color: "#707070",
+    border: "none",
+    borderRadius: "0",
+    borderBottom: "1px solid #006EC7",
   },
 });
 
